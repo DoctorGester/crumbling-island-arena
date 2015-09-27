a81b6db8714bacb2fe2f28278662e69c62075e97
shoujo <shoujo@localhost.localdomain>
2015-09-27 16:23:28 +0300

    Basic ES ability structure, lua remnant modifier

---
+++ game/scripts/vscripts/abilities/earth_spirit_q.lua
@@ -0,0 +1,6 @@
+earth_spirit_q = class({})
+
+function earth_spirit_q:OnSpellStart()
+	local caster = self:GetCaster()
+	local target = self:GetCursorPosition()
+end
---
+++ game/scripts/vscripts/abilities/earth_spirit_w.lua
@@ -0,0 +1,6 @@
+earth_spirit_w = class({})
+
+function earth_spirit_w:OnSpellStart()
+	local caster = self:GetCaster()
+	local target = self:GetCursorPosition()
+end
