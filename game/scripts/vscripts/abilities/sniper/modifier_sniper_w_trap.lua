3c0b2a8f99f284df995b1e2485ef344905deefee
doctorgester <doctorgester@gmail.com>
2015-12-01 04:48:16 +0300

    Cleaned up sniper W invis code

--- game/scripts/vscripts/abilities/sniper/modifier_sniper_w_trap.lua
+++ game/scripts/vscripts/abilities/sniper/modifier_sniper_w_trap.lua
@@ -2,7 +2,6 @@ modifier_sniper_w_trap = class({})
 
 function modifier_sniper_w_trap:OnCreated(keys)
     if IsServer() then
-        self.timePassed = 0
         self:StartIntervalThink(0.1)
     end
 end
@@ -11,40 +10,35 @@ function modifier_sniper_w_trap:CheckState()
     local state = {
         [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
         [MODIFIER_STATE_NO_HEALTH_BAR] = true,
-        [MODIFIER_STATE_UNSELECTABLE] = true
+        [MODIFIER_STATE_UNSELECTABLE] = true,
+        [MODIFIER_STATE_INVISIBLE] = self:GetElapsedTime() >= 0.3
     }
-
-    if IsServer() then
-        if self.timePassed >= 1.0 then
-            self.timePassed = -1
-            self:GetParent():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_invisible", { fadetime = 0.3 })
-        end
-
-        state[MODIFIER_STATE_INVISIBLE] = self.timePassed > 1.0 or self.timePassed == -1
-    end
-
+    
     return state
 end
 
 function modifier_sniper_w_trap:OnIntervalThink()
-    if IsServer() then
-        if self.timePassed ~= -1 then
-            self.timePassed = self.timePassed + 0.1
-        end
+    if self.timePassed ~= -1 then
+        self.timePassed = self.timePassed + 0.1
+    end
 
-        local hero = self:GetCaster().hero
-        local trap = self:GetParent()
+    local hero = self:GetCaster().hero
+    local trap = self:GetParent()
 
-        for _, target in pairs(Spells:GetValidTargets()) do
-            local distance = (target:GetPos() - trap:GetAbsOrigin()):Length2D()
+    for _, target in pairs(Spells:GetValidTargets()) do
+        local distance = (target:GetPos() - trap:GetAbsOrigin()):Length2D()
 
-            if target ~= hero and distance <= 64 and target:__instanceof__(Hero) then
-                self:GetParent():ForceKill(false)
-                target:AddNewModifier(hero, self:GetAbility(), "modifier_sniper_w", { duration = 1.7 })
-                ImmediateEffectPoint("particles/units/heroes/hero_techies/techies_stasis_trap_explode.vpcf", PATTACH_ABSORIGIN, trap, trap:GetAbsOrigin())
-                target:EmitSound("Arena.Sniper.HitW")
-                break
-            end
+        if target ~= hero and distance <= 64 and target:__instanceof__(Hero) then
+            self:GetParent():ForceKill(false)
+            target:AddNewModifier(hero, self:GetAbility(), "modifier_sniper_w", { duration = 1.7 })
+            ImmediateEffectPoint("particles/units/heroes/hero_techies/techies_stasis_trap_explode.vpcf", PATTACH_ABSORIGIN, trap, trap:GetAbsOrigin())
+            target:EmitSound("Arena.Sniper.HitW")
+            break
         end
     end
+end
+
+
+function modifier_sniper_r:GetModifierInvisibilityLevel(params)
+    return math.min(self:GetElapsedTime() / 0.3, 1.0)
 end
