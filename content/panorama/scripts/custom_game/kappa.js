567740b29f11da76c35323daff13bb42a511632e
doctorgester <doctorgester@gmail.com>
2016-06-06 02:35:41 +0300

    Added BTTV emote support

--- content/panorama/scripts/custom_game/kappa.js
+++ content/panorama/scripts/custom_game/kappa.js
@@ -150,7 +150,105 @@ var emotes = {
     "YouWHY": 4337
 };
 
+var bttvEmotes = {
+    "OhMyGoodness": "54fa925e01e468494b85b54d",
+    "PancakeMix": "54fa927801e468494b85b54e",
+    "PedoBear": "54fa928f01e468494b85b54f",
+    "PokerFace": "54fa92a701e468494b85b550",
+    "RageFace": "54fa92d701e468494b85b552",
+    "RebeccaBlack": "54fa92ee01e468494b85b553",
+    "aPliS": "54fa8f4201e468494b85b538",
+    "CiGrip": "54fa8fce01e468494b85b53c",
+    "CHAccepted": "54fa8fb201e468494b85b53b",
+    "FuckYea": "54fa90d601e468494b85b544",
+    "DatSauce": "54fa903b01e468494b85b53f",
+    "ForeverAlone": "54fa909b01e468494b85b542",
+    "GabeN": "54fa90ba01e468494b85b543",
+    "HailHelix": "54fa90f201e468494b85b545",
+    "HerbPerve": "54fa913701e468494b85b546",
+    "iDog": "54fa919901e468494b85b548",
+    "rStrike": "54fa930801e468494b85b554",
+    "ShoopDaWhoop": "54fa932201e468494b85b555",
+    "SwedSwag": "54fa9cc901e468494b85b565",
+    "M&Mjc": "54fab45f633595ca4c713abc",
+    "bttvNice": "54fab7d2633595ca4c713abf",
+    "TopHam": "54fa934001e468494b85b556",
+    "TwaT": "54fa935601e468494b85b557",
+    "WhatAYolk": "54fa93d001e468494b85b559",
+    "WatChuSay": "54fa99b601e468494b85b55d",
+    "aplis!": "54faa4d801e468494b85b576",
+    "Blackappa": "54faa50d01e468494b85b578",
+    "DogeWitIt": "54faa52f01e468494b85b579",
+    "BadAss": "54faa4f101e468494b85b577",
+    "SavageJerky": "54fb603201abde735115ddb5",
+    "Kaged": "54fbf11001abde735115de66",
+    "HHydro": "54fbef6601abde735115de57",
+    "TaxiBro": "54fbefeb01abde735115de5b",
+    "BroBalt": "54fbf00a01abde735115de5c",
+    "ButterSauce": "54fbf02f01abde735115de5d",
+    "BaconEffect": "54fbf05a01abde735115de5e",
+    "SuchFraud": "54fbf07e01abde735115de5f",
+    "CandianRage": "54fbf09c01abde735115de61",
+    "She'llBeRight": "54fbefc901abde735115de5a",
+    "OhhhKee": "54fbefa901abde735115de59",
+    "SexPanda": "5502874d135896936880fdd2",
+    "bttvWink": "550292c0135896936880fdef",
+    "bttvAngry": "550291a3135896936880fde3",
+    "bttvConfused": "550291be135896936880fde4",
+    "bttvCool": "550291d4135896936880fde5",
+    "bttvHappy": "55029200135896936880fde7",
+    "bttvSad": "5502925d135896936880fdea",
+    "bttvSleep": "55029272135896936880fdeb",
+    "bttvSurprised": "55029288135896936880fdec",
+    "bttvTongue": "5502929b135896936880fded",
+    "bttvUnsure": "550292ad135896936880fdee",
+    "bttvGrin": "550291ea135896936880fde6",
+    "bttvHeart": "55029215135896936880fde8",
+    "bttvTwink": "55029247135896936880fde9",
+    "VisLaud": "550352766f86a5b26c281ba2",
+    "SoSerious": "5514afe362e6bd0027aede8a",
+    "BatKappa": "550b6b07ff8ecee922d2a3e7",
+    "KaRappa": "550b344bff8ecee922d2a3c1",
+    "YetiZ": "55189a5062e6bd0027aee082",
+    "miniJulia": "552d2fc2236a1aa17a996c5b",
+    "motnahP": "55288e390fa35376704a4c7a",
+    "sosGame": "553b48a21f145f087fc15ca6",
+    "CruW": "55471c2789d53f2d12781713",
+    "RarePepe": "555015b77676617e17dd2e8e",
+    "iamsocal": "54fbef8701abde735115de58",
+    "haHAA": "555981336ba1901877765555",
+    "FeelsBirthdayMan": "55b6524154eefd53777b2580",
+    "RonSmug": "55f324c47f08be9f0a63cce0",
+    "KappaCool": "560577560874de34757d2dc0",
+    "Zappa": "5622aaef3286c42e57d8e4ab",
+    "SqShy": "5622ab523286c42e57d8e4b2",
+    "BasedGod": "566c9eeb65dbbdab32ec052b",
+    "bUrself": "566c9f3b65dbbdab32ec052e",
+    "ConcernDoge": "566c9f6365dbbdab32ec0532",
+    "FapFapFap": "566c9f9265dbbdab32ec0538",
+    "FeelsBadMan": "566c9fc265dbbdab32ec053b",
+    "FeelsGoodMan": "566c9fde65dbbdab32ec053e",
+    "FireSpeed": "566c9ff365dbbdab32ec0541",
+    "FishMoley": "566ca00f65dbbdab32ec0544",
+    "Hhhehehe": "566ca02865dbbdab32ec0547",
+    "KKona": "566ca04265dbbdab32ec054a",
+    "NaM": "566ca06065dbbdab32ec054e",
+    "OhGod": "566ca07965dbbdab32ec0552",
+    "PoleDoge": "566ca09365dbbdab32ec0555",
+    "tehPoleCat": "566ca11a65dbbdab32ec0558",
+    "AngelThump": "566ca1a365dbbdab32ec055b",
+    "SourPls": "566ca38765dbbdab32ec0560",
+    "LUL": "567b00c61ddbe1786688a633",
+    "SaltyCorn": "56901914991f200c34ffa656",
+    "FCreep": "56d937f7216793c63ec140cb",
+    "VapeNation": "56f5be00d48006ba34f530a4",
+    "ariW": "56fa09f18eff3b595e93ac26",
+    "notsquishY": "5709ab688eff3b595e93c595",
+    "DuckerZ": "573d38b50ffbf6cc5cc38dc9"
+};
+
 var template = "https://static-cdn.jtvnw.net/emoticons/v1/{image_id}/1.0";
+var bttvTemplate = "https://cdn.betterttv.net/emote/{image_id}/1x";
 
 function EscapeHtml(unsafe) {
     return unsafe
@@ -161,12 +259,20 @@ function EscapeHtml(unsafe) {
          .replace(/'/g, "&#039;");
  }
 
+function ProcessEmote(input, template, emote, id) {
+    var url = template.replace("{image_id}", id);
+    return input.replace(new RegExp("\\b" + emote + "\\b", "g"), "<img src='" + url + "'/>");
+}
+
 function InsertEmotes(input) {
     input = EscapeHtml(input);
 
     for (var emote in emotes) {
-        var url = template.replace("{image_id}", emotes[emote]);
-        input = input.replace(new RegExp("\\b" + emote + "\\b", "g"), "<img src='" + url + "'/>");
+        input = ProcessEmote(input, template, emote, emotes[emote])
+    }
+
+    for (var emote in bttvEmotes) {
+        input = ProcessEmote(input, bttvTemplate, emote, bttvEmotes[emote])
     }
 
     return input;
