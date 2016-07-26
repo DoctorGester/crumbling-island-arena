f2728155ee649fbbfdfdca829d6e80869210e105
doctorgester <doctorgester@gmail.com>
2016-07-26 03:44:28 +0300

    Added Tiny death animation

--- game/scripts/vscripts/heroes/tiny.lua
+++ game/scripts/vscripts/heroes/tiny.lua
@@ -7,3 +7,20 @@ function Tiny:SetUnit(unit)
         self:AttachWearable("models/heroes/tiny_01/tiny_01_"..part..".vmdl")
     end
 end
+
+function Tiny:Damage(...)
+    getbase(Tiny).Damage(self, ...)
+
+    if not self:Alive() then
+        for _, part in pairs(self.wearables) do
+            part:RemoveSelf()
+        end
+
+        self.wearables = {}
+
+        local index = ParticleManager:CreateParticle("particles/units/heroes/hero_tiny/tiny01_death.vpcf", PATTACH_CUSTOMORIGIN, nil)
+        ParticleManager:SetParticleControl(index, 0, self:GetPos())
+        ParticleManager:SetParticleControlForward(index, 0, self:GetFacing())
+        ParticleManager:ReleaseParticleIndex(index)
+    end
+end
