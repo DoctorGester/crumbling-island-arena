228a40d35ae165f50806abb4c57e8ad51f12850e
doctorgester <doctorgester@gmail.com>
2016-08-05 17:53:14 +0300

    Fixed Ember remnant not being able to use E

--- game/scripts/vscripts/abilities/ember/modifier_ember_r_visual.lua
+++ game/scripts/vscripts/abilities/ember/modifier_ember_r_visual.lua
@@ -6,8 +6,7 @@ function modifier_ember_r_visual:CheckState()
         [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
         [MODIFIER_STATE_NOT_ON_MINIMAP] = true,
         [MODIFIER_STATE_UNSELECTABLE] = true,
-        [MODIFIER_STATE_COMMAND_RESTRICTED] = true,
-        [MODIFIER_STATE_ROOTED] = true
+        [MODIFIER_STATE_COMMAND_RESTRICTED] = true
     }
 
     return state
