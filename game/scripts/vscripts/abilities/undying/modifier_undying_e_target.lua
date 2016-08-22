d8f866d0b74507f6ebdbc128f357e8f854872a73
doctorgester <doctorgester@gmail.com>
2016-08-22 17:52:11 +0300

    Removed slow from undying E

--- game/scripts/vscripts/abilities/undying/modifier_undying_e_target.lua
+++ game/scripts/vscripts/abilities/undying/modifier_undying_e_target.lua
@@ -25,18 +25,6 @@ if IsServer() then
     end
 end
 
-function self:DeclareFunctions()
-    local funcs = {
-        MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE
-    }
-
-    return funcs
-end
-
 function self:IsDebuff()
     return true
-end
-
-function self:GetModifierMoveSpeedBonus_Percentage(params)
-    return -15
 end
