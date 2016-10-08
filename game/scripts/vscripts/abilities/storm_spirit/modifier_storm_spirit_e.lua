38038f2e2a9c2e39a88d2c513794c2a6aae89619
doctorgester <doctorgester@gmail.com>
2016-10-08 07:26:25 +0300

    Storm Spirit is now invulnerable during his E

--- game/scripts/vscripts/abilities/storm_spirit/modifier_storm_spirit_e.lua
+++ game/scripts/vscripts/abilities/storm_spirit/modifier_storm_spirit_e.lua
@@ -1,20 +1,30 @@
 modifier_storm_spirit_e = class({})
 
-function modifier_storm_spirit_e:OnCreated(kv)
-    if IsServer() then
+if IsServer() then
+    function modifier_storm_spirit_e:OnCreated(kv)
         local index = ParticleManager:CreateParticle("particles/units/heroes/hero_stormspirit/stormspirit_ball_lightning.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetCaster())
         ParticleManager:SetParticleControlEnt(index, 1, self:GetCaster(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetCaster():GetOrigin(), true)
         self:AddParticle(index, false, false, -1, false, false)
+        self:GetParent():GetParentEntity():SetHidden(true)
+    end
+
+    function modifier_storm_spirit_e:OnDestroy()
+        self:GetParent():GetParentEntity():SetHidden(false)
     end
 end
 
 function modifier_storm_spirit_e:CheckState()
     local state = {
         [MODIFIER_STATE_STUNNED] = true,
-        [MODIFIER_STATE_NO_UNIT_COLLISION] = true
+        [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+        [MODIFIER_STATE_NO_HEALTH_BAR] = true
     }
 
     return state
+end
+
+function modifier_storm_spirit_e:IsInvulnerable()
+    return true
 end
 
 function modifier_storm_spirit_e:DeclareFunctions()
