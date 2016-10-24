cf81a2f8b608263ccd40105e3dd8bb56061d867d
doctorgester <doctorgester@gmail.com>
2016-10-24 03:49:10 +0300

    Added PA R sound

--- game/scripts/vscripts/abilities/pa/pa_r.lua
+++ game/scripts/vscripts/abilities/pa/pa_r.lua
@@ -6,4 +6,5 @@ function pa_r:OnSpellStart()
     local hero = self:GetCaster().hero
     hero:AddNewModifier(hero, self, "modifier_pa_r", { fadeTime = 0.7, duration = 5.0 })
     hero:EmitSound("Item.GlimmerCape.Activate")
+    hero:EmitSound("Arena.PA.CastR.Voice")
 end
