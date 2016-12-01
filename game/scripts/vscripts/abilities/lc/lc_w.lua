204dc74706123ef3fe56257c61d05fb46d78194b
doctorgester <doctorgester@gmail.com>
2016-12-02 02:40:40 +0300

    Legion Commander W can now be cast on the ground to target herself

--- game/scripts/vscripts/abilities/lc/lc_w.lua
+++ game/scripts/vscripts/abilities/lc/lc_w.lua
@@ -3,9 +3,17 @@ lc_w = class({})
 LinkLuaModifier("modifier_lc_w_shield", "abilities/lc/modifier_lc_w_shield", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_lc_w_speed", "abilities/lc/modifier_lc_w_speed", LUA_MODIFIER_MOTION_NONE)
 
+function lc_w:GetCastRange(loc, target)
+    if target == nil then
+        return 0
+    else
+        return self.BaseClass.GetCastRange(self, loc, target)
+    end
+end
+
 function lc_w:OnSpellStart()
     local hero = self:GetCaster():GetParentEntity()
-    local target = self:GetCursorTarget():GetParentEntity()
+    local target = self:GetCursorTarget() and self:GetCursorTarget():GetParentEntity() or hero
 
     target:AddNewModifier(hero, self, "modifier_lc_w_shield", { duration = 2 })
     target:EmitSound("Arena.LC.CastW")
