7b2b609fc3056fecf35aa7eaa5669da205ee8607
doctorgester <doctorgester@gmail.com>
2017-06-09 01:44:57 +0300

    Brewmaster W now correctly deals damage based on beer stacks on hero, fixes #5

--- game/scripts/vscripts/abilities/brew/brew_w.lua
+++ game/scripts/vscripts/abilities/brew/brew_w.lua
@@ -3,7 +3,7 @@ brew_w = class({})
 function brew_w:OnSpellStart()
     local hero = self:GetCaster().hero
     local target = self:GetCursorPosition()
-    local stacks = hero:FindAbility("brew_q"):CountBeer(hero)
+    local heroStacks = hero:FindAbility("brew_q"):CountBeer(hero)
 
     local projectile = DistanceCappedProjectile(hero.round, {
         ability = self,
@@ -12,14 +12,14 @@ function brew_w:OnSpellStart()
         to = target + Vector(0, 0, 128),
         speed = 1650,
         graphics = "particles/brew_w/brew_w.vpcf",
-        distance = 500 + 250 * stacks,
-        radius = 32 + 32 * stacks,
+        distance = 500 + 250 * heroStacks,
+        radius = 32 + 32 * heroStacks,
         hitSound = "Arena.Ember.HitQ",
         continueOnHit = true,
         hitFunction = function(projectile, target)
             local stacks = hero:FindAbility("brew_q"):CountBeer(target)
 
-            target:Damage(hero, stacks + 1)
+            target:Damage(heroStacks, stacks + 1)
 
             if stacks > 0 then
                 target:AddNewModifier(hero, self, "modifier_stunned_lua", { duration = stacks * 0.5 })
