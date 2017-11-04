f23e90e8f871c79fcf12785e075e9924450f50b0
doctorgester <doctorgester@gmail.com>
2017-11-04 17:55:35 +0300

    Fixed QoP R + Q healing being random, closes #257

--- game/scripts/vscripts/abilities/qop/modifier_qop_q.lua
+++ game/scripts/vscripts/abilities/qop/modifier_qop_q.lua
@@ -18,9 +18,12 @@ if IsServer() then
     function modifier_qop_q:OnIntervalThink()
         self:GetParent():GetParentEntity():Damage(self:GetCaster().hero, self:GetAbility():GetDamage() / 3)
 
-        if self.heals and self:GetCaster():GetParentEntity():Alive() and RandomInt(0, 1) == 0 then
-            self:GetCaster().hero:Heal(self:GetAbility():GetDamage() / 3)
-            self:GetCaster():EmitSound("Arena.QOP.CastR.Heal")
+        if self.heals and self:GetCaster():GetParentEntity():Alive() then
+            self:GetCaster():GetParentEntity():Heal(self:GetAbility():GetDamage() / 3)
+
+            if RandomInt(0, 1) == 0 then
+                self:GetCaster():EmitSound("Arena.QOP.CastR.Heal")
+            end
         end
     end
 end
