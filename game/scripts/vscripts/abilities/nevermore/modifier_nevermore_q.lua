c30ccce1c90073d5e55fdf04a046218e44455cae
vocalpocal <puuppamies@hotmail.com>
2018-08-10 02:39:51 +0300

    Shadow fiend recast fix (#373)
    Fixes #356

--- game/scripts/vscripts/abilities/nevermore/modifier_nevermore_q.lua
+++ game/scripts/vscripts/abilities/nevermore/modifier_nevermore_q.lua
@@ -4,7 +4,23 @@ function modifier_nevermore_q:IsHidden()
     return true
 end
 
+function modifier_nevermore_q:DestroyOnExpire()
+    return false
+end
+
 if IsServer() then
+    function modifier_nevermore_q:OnCreated()
+        self:StartIntervalThink(0)
+        self:OnIntervalThink()
+    end
+
+    function modifier_nevermore_q:OnIntervalThink()
+        local ability = self:GetAbility()
+        if self:GetRemainingTime() <= 0 and not ability:IsInAbilityPhase()then
+            self:Destroy()
+        end
+    end
+
     function modifier_nevermore_q:OnDestroy()
         self:GetAbility():StartCooldown(self:GetAbility():GetCooldown(1))
     end
