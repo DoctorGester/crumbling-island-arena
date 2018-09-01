73a6eb8f21e9167ed36093efe727d0f7c12fd1b7
doctorgester <doctorgester@gmail.com>
2018-09-01 16:27:05 +0300

    Void Q proc moved to post cast, root duration 1.0s -> 1.5s

--- game/scripts/vscripts/abilities/void/modifier_void_q.lua
+++ game/scripts/vscripts/abilities/void/modifier_void_q.lua
@@ -1,11 +1,7 @@
 modifier_void_q = class({})
 
 if IsServer() then
-    function modifier_void_q:OnAbilityImmediate(event)
-        self:OnAbilityStart(event)
-    end
-
-    function modifier_void_q:OnAbilityStart(event)
+    function modifier_void_q:OnAbilityExecuted(event)
         local target = event.unit:GetParentEntity()
         local shouldBeAffectedByAbility = target:FindModifier("modifier_void_q")
 
@@ -14,14 +10,14 @@ if IsServer() then
         end
 
         if shouldBeAffectedByAbility then
-            target:AddNewModifier(target, self:GetAbility(), "modifier_void_q_root", { duration = 1.0 })
+            target:AddNewModifier(target, self:GetAbility(), "modifier_void_q_root", { duration = 1.5 })
         end
     end
 end
 
 function modifier_void_q:DeclareFunctions()
     local funcs = {
-        MODIFIER_EVENT_ON_ABILITY_START
+        MODIFIER_EVENT_ON_ABILITY_EXECUTED
     }
 
     return funcs
